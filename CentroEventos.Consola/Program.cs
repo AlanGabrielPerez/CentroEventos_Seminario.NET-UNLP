434dc861754924c26b37e466272ea6b363870274
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2025-05-17 14:20:53 -0300

    Test crearPersona

--- CentroEventos.Consola/Program.cs
+++ CentroEventos.Consola/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CentroEventos.Aplicacion.Entidades;
+using CentroEventos.Aplicacion.Interfaces;
+using CentroEventos.Aplicacion.Validadores;
+using CentroEventos.Aplicacion.CasosDeUso;
+using CentroEventos.Repositorios;
+
+var persona = new Persona
+{
+    Id = 1,
+    DNI = "222920",
+    Nombre = "Alan",
+    Apellido = "Perez",
+    Email = "alanPerez@mail.com",
+    Telefono = "221222920"
+};
+
+var personaRepo = new RepositorioPersona();
+var validador = new PersonaValidador();
+var useCase = new CrearPersonaUseCase();
+
+try
+{
+    useCase.Ejecutar(persona, personaRepo, validador);
+    Console.WriteLine("Persona creada con éxito.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
